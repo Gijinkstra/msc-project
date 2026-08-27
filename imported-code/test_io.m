f6add86bbbac6a928579e4cb806d1af98d18a670
ali mac <adempsey97@outlook.com>
2026-08-27 07:19:21 +0100

    Updates methods and adding measurement data

--- imported-code/test_io.m
+++ imported-code/test_io.m
@@ -4,7 +4,7 @@ clear;
 fs = 44100;
 % framelength = 1024;
 
-dev = audioDeviceReader('Device', 'io|2');
+dev = audioDeviceReader('Device', "io|2");
 set(dev,'NumChannels',2)
 setup(dev);
 
@@ -26,3 +26,23 @@ y = audioread('test.wav');
 figure(1);
 clf;
 plot(y);
+
+impactHammerSignal = y(:, 2);
+celloBodySignal = y(:, 1);
+
+[hammerFft, freqAx] = singleSidedFft(impactHammerSignal, fs);
+[celloFft, freqAxC] = singleSidedFft(celloBodySignal, fs);
+
+figure(2)
+tiledlayout(2, 1)
+t1 = nexttile;
+hold(t1, "on")
+plot(celloBodySignal, 'LineWidth', 1.1, 'DisplayName', 'Cello body signal')
+plot(impactHammerSignal, 'LineWidth', 1.1, 'DisplayName', 'Impact hammer signal')
+
+t2 = nexttile;
+hold(t2, "on")
+plot(freqAxC, 20*log10(celloFft), 'LineWidth', 1.1, 'DisplayName', 'Cello body signal')
+% plot(freqAx, 20*log10(hammerFft), 'LineWidth', 1.1, 'DisplayName', 'Impact hammer signal')
+
+soundsc(celloBodySignal, fs)
